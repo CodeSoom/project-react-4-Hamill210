ee8fbb5bbc96c722d84c421f3a7afe44f3dc4301
최한울 <hanul.choi95@nts-corp.com>
2021-06-30 00:35:09 +0900

    Confirm auto deploy
    - push만 하면 자동으로 배포되는지 확인하기 위한 커밋입니다.

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/hello', (req, res) => {
-  res.status(200).send('Hello CodeSoom!!!!');
+  res.status(200).send('즐거운 CI/CD 시간!!!!');
 });
 
 app.listen(3010, () => {
