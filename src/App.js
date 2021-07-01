7e3e2afd4467702e7a74477b1c46eaf1f873165a
최한울 <hanul.choi95@nts-corp.com>
2021-07-02 00:27:01 +0900

    Install eslint

--- src/App.js
+++ src/App.js
@@ -3,22 +3,21 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      username: null,
+      appName: null,
     };
   }
 
   componentDidMount() {
     fetch('api/group')
-      .then(res => res.json())
-      .then(data => this.setState({ appName: data.appName }));
+      .then((res) => res.json())
+      .then((data) => this.setState({ appName: data.appName }));
   }
 
   render() {
-    const {appName} = this.state;
+    const { appName } = this.state;
 
     return (
       <div className="App">
