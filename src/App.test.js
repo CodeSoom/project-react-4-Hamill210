893e31100c3ab669f6a5103142a1c6b1377e2a5d
최한울 <hanul.choi95@nts-corp.com>
2021-07-05 00:48:53 +0900

    헤더에 대한 테스트를 작성한다

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  it('renders header', () => {
+    const { queryByText } = render(<App />);
+    expect(queryByText('SOOM.GG')).not.toBeNull();
+  });
 });
