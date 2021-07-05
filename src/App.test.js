b845ffd86b26e31a221cbe87ce5ef86c32ec8df5
최한울 <hanul.choi95@nts-corp.com>
2021-07-06 00:37:25 +0900

    검색창에 대한 테스트를 작성한다
    - 기본적인 input 창 생성

--- src/App.test.js
+++ src/App.test.js
@@ -9,4 +9,9 @@ describe('App', () => {
     const { queryByText } = render(<App />);
     expect(queryByText('SOOM.GG')).not.toBeNull();
   });
+
+  it('renders Search Area', () => {
+    const { queryByPlaceholderText } = render(<App />);
+    expect(queryByPlaceholderText('소환사명을 입력하세요.')).not.toBeNull();
+  });
 });
