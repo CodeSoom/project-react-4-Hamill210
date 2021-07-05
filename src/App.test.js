35704f31cc4b06f0adf77d147c2c9b83a3618679
최한울 <hanul.choi95@nts-corp.com>
2021-07-06 07:33:56 +0900

    검색창 버튼에 대한 테스트를 작성한다

--- src/App.test.js
+++ src/App.test.js
@@ -24,4 +24,13 @@ describe('App', () => {
       target: { value: '최하누리' },
     });
   });
+
+  it('renders search button', () => {
+    const { queryByText } = renderApp();
+    const buttonElement = queryByText('검색');
+
+    expect(buttonElement).not.toBeNull();
+
+    fireEvent.click(buttonElement);
+  });
 });
