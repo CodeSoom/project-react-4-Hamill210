6a4637450933b25796e47e601f7750740093c01f
최한울 <hanul.choi95@nts-corp.com>
2021-07-07 23:51:57 +0900

    setSummoner에 대한 테스트를 작성한다

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,4 +1,7 @@
-import reducer, { setPlayerName } from './slice';
+import reducer, {
+  setPlayerName,
+  setSummoner,
+} from './slice';
 
 describe('reducer', () => {
   context('when previous state is undefined', () => {
@@ -26,6 +29,26 @@ describe('reducer', () => {
       const state = reducer(initialState, setPlayerName(userName));
 
       expect(state.player.userName).toEqual('최하누리');
+    });
+  });
+
+  describe('setSummoner', () => {
+    it('changes summoner information', () => {
+      const initialState = {
+        summoner: {},
+      };
+      const summoner = {
+        id: 'ROmqWcUq7lVH-iIBcatiskiO9leUlsD5SL9VQXkXPFO_Nw',
+        accountId: 'EUIJaJrzXK_sB9qfTCL7eRPIAagzFh9K9esUkb2G1Vze',
+        puuid: 'MBsxsPWzxruXkjSBghzf79BermZyogtFgrLgd7QJUS5iSXfYG2ovL7NA2yiOXNZGKdmSxZxzT9WgJQ',
+        name: '최하누리',
+        profileIconId: 4574,
+        revisionDate: 1624691287000,
+        summonerLevel: 289,
+      };
+      const state = reducer(initialState, setSummoner(summoner));
+
+      expect(state.summoner.name).toEqual('최하누리');
     });
   });
 });
