bcb3d3b39727b7a595a5c6ebebf24f568d470571
최한울 <hanul.choi95@nts-corp.com>
2021-07-09 01:33:36 +0900

    플레이어 이름으로 검색 시 랭크 데이터 정보 추가

---
+++ fixtures/ranks.js
@@ -0,0 +1,34 @@
+const ranks = [
+  {
+    leagueId: '1',
+    queueType: 'RANKED_FLEX_SR',
+    tier: 'PLATINUM',
+    rank: 'IV',
+    summonerId: '1',
+    summonerName: '최하누리',
+    leaguePoints: 1,
+    wins: 24,
+    losses: 16,
+    veteran: false,
+    inactive: false,
+    freshBlood: true,
+    hotStreak: false
+  },
+  {
+    leagueId: '2',
+    queueType: 'RANKED_SOLO_5x5',
+    tier: 'DIAMOND',
+    rank: 'II',
+    summonerId: '2',
+    summonerName: '최하누리',
+    leaguePoints: 75,
+    wins: 145,
+    losses: 132,
+    veteran: false,
+    inactive: false,
+    freshBlood: false,
+    hotStreak: false
+  },
+];
+
+export default ranks;
--- src/components/PlayerInfo.jsx
+++ src/components/PlayerInfo.jsx
@@ -1,4 +1,17 @@
-export default function PlayerInfo({ name, summonerLevel }) {
+import { memo } from 'react';
+
+function PlayerInfo({
+  name,
+  summonerLevel,
+  soloRank,
+  subRank,
+}) {
+  if (!name) {
+    return (
+      <p>소환사명을 검색해주세요.</p>
+    );
+  }
+
   return (
     <>
       <div>
@@ -11,6 +24,28 @@ export default function PlayerInfo({ name, summonerLevel }) {
         {' : '}
         {summonerLevel}
       </div>
+      <div>
+        솔로랭크
+        {' : '}
+        {`${soloRank.tier} ${soloRank.rank} ${soloRank.leaguePoints}`}
+      </div>
+      <div>
+        전적
+        {' : '}
+        {`${soloRank.wins}승 ${soloRank.losses}패`}
+      </div>
+      <div>
+        자유랭크
+        {' : '}
+        {`${subRank.tier} ${subRank.rank} ${subRank.leaguePoints}`}
+      </div>
+      <div>
+        전적
+        {' : '}
+        {`${subRank.wins}승 ${subRank.losses}패`}
+      </div>
     </>
   );
 }
+
+export default memo(PlayerInfo);
