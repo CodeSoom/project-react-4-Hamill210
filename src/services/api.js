96348afc5014fd0f8c840acf9575e6d044d9718a
최한울 <hanul.choi95@nts-corp.com>
2021-07-08 23:42:09 +0900

    riot API key 환경변수 추가하여 사용

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,9 @@
-import X_RIOT_TOKEN from '../config/riotKey';
-
 export async function fetchSummoners(username) {
   const response = await fetch(`lol/summoner/v4/summoners/by-name/${username}`, {
     method: 'GET',
     headers: {
       Origin: 'https://developer.riotgames.com',
-      'X-Riot-Token': X_RIOT_TOKEN,
+      'X-Riot-Token': process.env.REACT_APP_X_RIOT_TOKEN,
     },
   });
 
