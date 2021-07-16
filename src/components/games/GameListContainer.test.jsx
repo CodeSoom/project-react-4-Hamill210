ac21e23da45144caaa05ee2b4abc6eb3cdec4f92
최한울 <hanul.choi95@nts-corp.com>
2021-07-17 02:59:13 +0900

    실패하던 테스트 수정
    - 구현부 코드 수정하면서 테스트 코드를 같이 수정함

--- src/components/games/GameListContainer.test.jsx
+++ src/components/games/GameListContainer.test.jsx
@@ -4,8 +4,9 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import GameListContainer from './GameListContainer';
 
-import MATCH_INFOS from '../../../fixtures/matchInfos'
-import GAME_INFOS from '../../../fixtures/gameInfos'
+import SUMMONER from '../../../fixtures/summoner';
+import MATCH_INFOS from '../../../fixtures/matchInfos';
+import GAME_INFOS from '../../../fixtures/gameInfos';
 
 jest.mock('react-redux');
 
@@ -21,6 +22,9 @@ describe('GameListContainer', () => {
         matchInfos: MATCH_INFOS,
         gameInfos: GAME_INFOS,
       },
+      player: {
+        summoner: SUMMONER,
+      },
     }));
   });
 
@@ -29,29 +33,28 @@ describe('GameListContainer', () => {
       <GameListContainer />,
     );
 
-    expect(container).toHaveTextContent('솔랭')
-    expect(container).toHaveTextContent('21시간 전')
-    expect(container).toHaveTextContent('패배')
-    expect(container).toHaveTextContent('34분 11초')
+    expect(container).toHaveTextContent('솔랭');
+    expect(container).toHaveTextContent('패배');
+    expect(container).toHaveTextContent('34분 11초');
 
-    expect(container).toHaveTextContent('카밀')
+    expect(container).toHaveTextContent('카밀');
 
-    expect(container).toHaveTextContent('12 / 9 / 10')
-    expect(container).toHaveTextContent('2.44:1 평점')
+    expect(container).toHaveTextContent('12 / 9 / 10');
+    expect(container).toHaveTextContent('2.44:1 평점');
 
-    expect(container).toHaveTextContent('레벨')
-    expect(container).toHaveTextContent('181 CS')
+    expect(container).toHaveTextContent('레벨');
+    expect(container).toHaveTextContent('181 (5.3) CS');
 
-    expect(container).toHaveTextContent('최하누리')
-    expect(container).toHaveTextContent('Returned Player')
-    expect(container).toHaveTextContent('피곤한 문화인')
-    expect(container).toHaveTextContent('베 2')
-    expect(container).toHaveTextContent('Airsalt')
+    expect(container).toHaveTextContent('최하누리');
+    expect(container).toHaveTextContent('Returned Player');
+    expect(container).toHaveTextContent('피곤한 문화인');
+    expect(container).toHaveTextContent('베 2');
+    expect(container).toHaveTextContent('Airsalt');
 
-    expect(container).toHaveTextContent('문라딘')
-    expect(container).toHaveTextContent('0975310849853211')
-    expect(container).toHaveTextContent('머머리협회사현')
-    expect(container).toHaveTextContent('흰머리오목눈이3')
-    expect(container).toHaveTextContent('단발이 끌리더라')
+    expect(container).toHaveTextContent('문라딘');
+    expect(container).toHaveTextContent('0975310849853211');
+    expect(container).toHaveTextContent('머머리협회사현');
+    expect(container).toHaveTextContent('흰머리오목눈이3');
+    expect(container).toHaveTextContent('단발이 끌리더라');
   });
 });
--- src/components/player/PlayerInfo.test.jsx
+++ src/components/player/PlayerInfo.test.jsx
@@ -4,10 +4,15 @@ import PlayerInfo from './PlayerInfo';
 
 describe('PlayerInfo', () => {
   it('renders', () => {
+    const summoner = {
+      name: '최하누리',
+      summonerLevel: '30',
+    }
+
     const soloRank = {
       tier: '다이아몬드',
       rank: '2',
-      leaguePoints: '75LP',
+      leaguePoints: '75',
       wins: '145',
       losses: '132',
     }
@@ -15,36 +20,29 @@ describe('PlayerInfo', () => {
     const subRank = {
       tier: '플레티넘',
       rank: '4',
-      leaguePoints: '1LP',
+      leaguePoints: '1',
       wins: '24',
       losses: '16',
     }
 
     const { container } = render(
       <PlayerInfo
-        name="최하누리"
-        summonerLevel={30}
+        summoner={summoner}
         soloRank={soloRank}
         subRank={subRank}
       />,
     );
 
-    expect(container).toHaveTextContent('소환사명');
     expect(container).toHaveTextContent('최하누리');
 
-    expect(container).toHaveTextContent('레벨');
-    expect(container).toHaveTextContent('30');
-
     expect(container).toHaveTextContent('솔로랭크');
-    expect(container).toHaveTextContent('다이아몬드 2 75LP');
-
-    expect(container).toHaveTextContent('전적');
+    expect(container).toHaveTextContent('다이아몬드 2');
+    expect(container).toHaveTextContent('75 LP');
     expect(container).toHaveTextContent('145승 132패');
 
     expect(container).toHaveTextContent('자유랭크');
-    expect(container).toHaveTextContent('플레티넘 4 1LP');
-
-    expect(container).toHaveTextContent('전적');
+    expect(container).toHaveTextContent('플레티넘 4');
+    expect(container).toHaveTextContent('1 LP');
     expect(container).toHaveTextContent('24승 16패');
   });
 });
