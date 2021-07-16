f390ec22ad6b7e3fb82d5c985488fb969fbf690c
최한울 <hanul.choi95@nts-corp.com>
2021-07-16 23:30:24 +0900

    전체 레이아웃을 수평정렬로 맞춘다
    - 컨텐츠를 수평정렬로 맞춥니다

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
+import styled from '@emotion/styled';
+
 import SearchBoxContainer from './components/search/SearchBoxContainer';
 import PlayerInfoContainer from './components/player/PlayerInfoContainer';
 import GameListContainer from './components/games/GameListContainer';
 
+const Wrapper = styled.div({
+  minHeight: '100%',
+});
+
+const Content = styled.div({
+  width: '1000px',
+  margin: 'auto',
+});
+
 function App() {
   return (
-    <>
+    <Wrapper>
       <h2>SOOM.GG</h2>
       <SearchBoxContainer />
-      <PlayerInfoContainer />
-      <br />
-      <GameListContainer />
-    </>
+      <Content>
+        <PlayerInfoContainer />
+        <br />
+        <GameListContainer />
+      </Content>
+    </Wrapper>
   );
 }
 
