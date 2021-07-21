47ab7a51b0c23ee7c04d9df267dd29f78871fe5d
최한울 <hanul.choi95@nts-corp.com>
2021-07-22 05:00:09 +0900

    검색창 스타일 수정
    - 좀더 보기 좋은 모양으로 스타일링

--- src/components/search/SearchBox/SearchBox.jsx
+++ src/components/search/SearchBox/SearchBox.jsx
@@ -18,20 +18,30 @@ function SearchBox({ userName, onChangeValue, onClickSearchPlayer }) {
 
   return (
     <Styled.Div.Container>
-      <input
-        ref={inputRef}
-        type="text"
-        placeholder="소환사명을 입력하세요."
-        value={userName}
-        onChange={handleChange}
-        onKeyPress={handleKeyPressEnter}
-      />
-      <button
-        type="button"
-        onClick={onClickSearchPlayer}
-      >
-        검색
-      </button>
+      <Styled.Div.CountryContent>
+        <Styled.Span.Country>
+          KR
+        </Styled.Span.Country>
+      </Styled.Div.CountryContent>
+      <div>
+        <Styled.Input.TextArea
+          ref={inputRef}
+          type="text"
+          placeholder="소환사명을 입력하세요."
+          value={userName}
+          onChange={handleChange}
+          onKeyPress={handleKeyPressEnter}
+        />
+        <Styled.Button.SearchBtn
+          type="button"
+          onClick={onClickSearchPlayer}
+        >
+          <Styled.Img.SearchImg
+            src="https://opgg-static.akamaized.net/images/gnb/svg/00-icon-gg.svg"
+            alt="Search Button"
+          />
+        </Styled.Button.SearchBtn>
+      </div>
     </Styled.Div.Container>
   );
 }
--- src/components/search/SearchBox/style.js
+++ src/components/search/SearchBox/style.js
@@ -2,12 +2,59 @@ import styled from '@emotion/styled';
 
 export const Div = {
   Container: styled.div({
-    display: 'grid',
-    gridTemplateColumns: 'auto 50px',
+    display: 'flex',
     position: 'relative',
-    height: '30px',
+    height: '32px',
     borderRadius: '4px',
+  }),
+  CountryContent: styled.div({
+    display: 'flex',
+    position: 'relative',
+    alignItems: 'center',
+    background: '#ecf2ff',
+    color: '#4171d6',
+    fontSize: '12px',
+    padding: '0 12px',
+    borderRadius: '4px 0 0 4px',
   }),
 };
 
-export const temp = '';
+export const Span = {
+  Country: styled.span({
+    margin: '6px',
+  }),
+};
+
+export const Input = {
+  TextArea: styled.input({
+    width: '256px',
+    height: '100%',
+    padding: '9px 12px',
+    border: 'none',
+    background: '#fff',
+    fontSize: '12px',
+    color: '#727272',
+    outline: 'none',
+    boxSizing: 'border-box',
+  }),
+};
+
+export const Button = {
+  SearchBtn: styled.button({
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    width: '54px',
+    height: '100%',
+    textAlign: 'center',
+    background: '#fff',
+    border: 'none',
+  }),
+};
+
+export const Img = {
+  SearchImg: styled.img({
+    width: '100%',
+    height: '100%',
+  }),
+};
