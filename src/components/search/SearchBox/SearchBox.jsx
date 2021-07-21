2f457f55b4c467312c5cf9d94f0d52b9caa0fd2c
최한울 <hanul.choi95@nts-corp.com>
2021-07-22 04:03:07 +0900

    검색창에서 enter 로 소환사 검색 가능
    - enter 눌러 소환사 검색 가능
    - 검색 후 input창 blur 추가

--- src/components/search/SearchBox/SearchBox.jsx
+++ src/components/search/SearchBox/SearchBox.jsx
@@ -1,19 +1,30 @@
-import { memo } from 'react';
+import { memo, useRef } from 'react';
 import * as Styled from './style';
 
 function SearchBox({ userName, onChangeValue, onClickSearchPlayer }) {
+  const inputRef = useRef(null);
+
   const handleChange = (event) => {
     const { target: { value } } = event;
     onChangeValue(value);
   };
 
+  const handleKeyPressEnter = (event) => {
+    if (event.key === 'Enter') {
+      onClickSearchPlayer();
+      inputRef.current.blur();
+    }
+  };
+
   return (
     <Styled.Div.Container>
       <input
+        ref={inputRef}
         type="text"
         placeholder="소환사명을 입력하세요."
         value={userName}
         onChange={handleChange}
+        onKeyPress={handleKeyPressEnter}
       />
       <button
         type="button"
